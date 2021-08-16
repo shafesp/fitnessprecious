e6fd1b7c113bb5d26eaabcaf0548f4da45bf448b
shafe pahochiya <65221883+shafesp@users.noreply.github.com>
2021-08-16 10:31:22 +0530

    Update connection.inc.php

--- connection.inc.php
+++ connection.inc.php
@@ -2,7 +2,7 @@
 session_start();
 $con=mysqli_connect("remotemysql.com","dkJk4i7bcS","l0XRA3aDPC","dkJk4i7bcS");
 define('SERVER_PATH',$_SERVER['DOCUMENT_ROOT'].'/php/ecom/');
-define('SITE_PATH','http://127.0.0.1/php/ecom/');
+define('SITE_PATH','https://fitness-precious.herokuapp.com/');
 
 define('PRODUCT_IMAGE_SERVER_PATH',SERVER_PATH.'media/product/');
 define('PRODUCT_IMAGE_SITE_PATH',SITE_PATH.'media/product/');
